0956ff785cc49432c700b3224ceac397c2cf6594
matt barbier <matthewdavidbarbier@gmail.com>
2018-08-11 22:20:14 +0100

    Added search example to scrape

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import urllib.request
 
-url = 'https://store.steampowered.com/app/728880/Overcooked_2/'
+#url = 'https://store.steampowered.com/app/728880/Overcooked_2/'
+url = 'https://store.steampowered.com/search/?term=overcooked+2'
 response = urllib.request.urlopen(url)
 data = response.read()      # a `bytes` object
 text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
