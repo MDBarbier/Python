6f568ab7b6d2ac7d6cebe9cdd470dba2716de695
matt barbier <matthewdavidbarbier@gmail.com>
2018-08-13 11:25:18 +0100

    removed pycharm stuff, added notes to scraper

--- scrape.py
+++ scrape.py
@@ -6,3 +6,11 @@ response = urllib.request.urlopen(url)
 data = response.read()      # a `bytes` object
 text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
 print(text)
+
+
+
+#As an alternative to screenscraping, you can use https://store.steampowered.com/api/appdetails?appids=320240 to get a JSON object back containing the data about a game
+
+#http://api.steampowered.com/ISteamApps/GetAppList/v0002/ gets a list of all games - if loaded this into a collection could match name against it to get steam app id
+
+#could still use screenscrape to add games from other sources
