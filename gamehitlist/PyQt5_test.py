f64656792296a772a15f2d5c39fa27e01aff4149
MDBarbier <matthew-7.7@hotmail.com>
2018-09-22 12:03:33 +0100

    pyqt demo

--- gamehitlist/PyQt5_test.py
+++ gamehitlist/PyQt5_test.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+ 
 
 class App(QMainWindow):
 
@@ -17,7 +19,15 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.statusBar().showMessage('Message in statusbar.')
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This is an example button')
+        button.move(100,70)
+        button.clicked.connect(self.on_click)
         self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
