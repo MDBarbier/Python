18a234a94ae1f38c1bf31e1cbe99c4d48254580b
Matt Barbier <matthewdavidbarbier@gmail.com>
2019-11-07 15:54:38 +0000

    added python postgres backup script

---
+++ postgres/backup.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+conn = psycopg2.connect("dbname=gamemanager user=postgres password=spectrum host=riker")
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM games;")
+
+
+records = cur.fetchall()
+
+print(records)
+
+# Make the changes to the database persistent
+conn.commit()
+
+cur.close()
+conn.close()
